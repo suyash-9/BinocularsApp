8f85085d6b0a2e645ccec16552624dd6cbbba579
Suyash Sinha <sinha.suyash.ss@gmail.com>
2021-09-05 19:58:35 +0530

    RecyclerView Commit

--- app/src/main/java/com/example/binoculars/CameraActivity.kt
+++ app/src/main/java/com/example/binoculars/CameraActivity.kt
@@ -1,9 +1,11 @@
 package com.example.binoculars
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -18,67 +20,21 @@ class CameraActivity : AppCompatActivity() {
     var preview:Preview?=null
     var imagecapture:ImageCapture?=null
     //var cameraSelector:CameraSelector?=null
+    companion object{
+        @JvmStatic val PHOTO_REQ_CODE=234
+        @JvmStatic val EXTRA_DATA="data"
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera2)
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==PERMISSION_GRANTED){
-            startCamera()
+
+        takepicbtn.setOnClickListener {
+                val takephotointent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takephotointent, PHOTO_REQ_CODE)
         }
-        else{
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),0)
-        }
-        capturebtn.setOnClickListener {
-            takePhoto()
-        }
-    }
-
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==PERMISSION_GRANTED){
-            startCamera()
-        }
-        else{
-            Toast.makeText(this,"Please accept the permission",Toast.LENGTH_LONG).show()
-        }
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture=ProcessCameraProvider.getInstance(this)
-        cameraProviderFuture.addListener(Runnable {
-               val cameraProvider=cameraProviderFuture.get()
-            preview=Preview.Builder().build()
-            preview?.setSurfaceProvider(cameraView.getSurfaceProvider())
-            imagecapture=ImageCapture.Builder().build()
-            val cameraSelector=CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-            cameraProvider.unbindAll()
-            camera=cameraProvider.bindToLifecycle(this,cameraSelector,preview,imagecapture)
-
-        },ContextCompat.getMainExecutor(this))
-    }
-
-    private fun takePhoto() {
-        val photofile= File(externalMediaDirs.firstOrNull(),"Binoculars - ${System.currentTimeMillis()}.jpg")
-        val output=ImageCapture.OutputFileOptions.Builder(photofile).build()
-        imagecapture?.takePicture(output,ContextCompat.getMainExecutor(this),object :ImageCapture.OnImageSavedCallback{
-            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                Toast.makeText(applicationContext,"Image Saved",Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onError(exception: ImageCaptureException) {
-                TODO("Not yet implemented")
-            }
-
-        })
 
     }
 }
--- app/src/main/java/com/example/binoculars/MainActivity.kt
+++ app/src/main/java/com/example/binoculars/MainActivity.kt
@@ -4,14 +4,14 @@ import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.binoculars.barcode.BarcodeActivity
 import com.example.binoculars.facedetect.FaceDetectActivity
 import com.example.binoculars.imagelabeler.ImageLabelingActivity
 import com.example.binoculars.textrecognition.TextRecognitionActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),RecyclerAdapter.OnItemClickListener {
     //lateinit var str:String
     companion object{
         @JvmStatic val PHOTO_REQ_CODE=234
@@ -24,37 +24,69 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        takepicbtn.setOnClickListener {
-                val takephotointent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(takephotointent, PHOTO_REQ_CODE)
-        }
 
-        buttonActivitybtn.setOnClickListener {
-            val intent=Intent(this,CameraActivity2::class.java)
-            //intent.putExtra("Ans",str)
+        binocularRV.layoutManager=LinearLayoutManager(this)
+        val adapter=RecyclerAdapter(this)
+        binocularRV.adapter=adapter
+
+
+
+
+
+//        takepicbtn.setOnClickListener {
+//                val takephotointent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            startActivityForResult(takephotointent, PHOTO_REQ_CODE)
+//        }
+//
+//        buttonActivitybtn.setOnClickListener {
+//            val intent=Intent(this,CameraActivity2::class.java)
+//            //intent.putExtra("Ans",str)
+//            startActivity(Intent(this,CameraActivity2::class.java))
+//        }
+//        btnBarcodeActivity.setOnClickListener {
+//            startActivity(Intent(this, BarcodeActivity::class.java))
+//        }
+//        btnFaceDetectActivity.setOnClickListener {
+//            startActivity(Intent(this, FaceDetectActivity::class.java))
+//        }
+//        btnLabelerActivity.setOnClickListener {
+//            startActivity(Intent(this, ImageLabelingActivity::class.java))
+//        }
+//        btnTextRecogActivity.setOnClickListener {
+//            startActivity(Intent(this, TextRecognitionActivity::class.java))
+//        }
+    }
+
+    override fun onItemClick(position: Int) {
+        if(position==0){
+            startActivity(Intent(this,CameraActivity::class.java))
+        }
+        if(position==1){
             startActivity(Intent(this,CameraActivity2::class.java))
         }
-        btnBarcodeActivity.setOnClickListener {
-            startActivity(Intent(this, BarcodeActivity::class.java))
-        }
-        btnFaceDetectActivity.setOnClickListener {
+        if(position==2){
             startActivity(Intent(this, FaceDetectActivity::class.java))
         }
-        btnLabelerActivity.setOnClickListener {
+        if(position==3){
+            startActivity(Intent(this, TextRecognitionActivity::class.java))
+        }
+        if(position==4){
             startActivity(Intent(this, ImageLabelingActivity::class.java))
         }
-        btnTextRecogActivity.setOnClickListener {
-            startActivity(Intent(this, TextRecognitionActivity::class.java))
+        if(position==5){
+            startActivity(Intent(this, BarcodeActivity::class.java))
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == PHOTO_REQ_CODE && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get(EXTRA_DATA) as Bitmap
-            image.setImageBitmap(imageBitmap)
+            //image.setImageBitmap(imageBitmap)
             return
         }
 
         super.onActivityResult(requestCode, resultCode, data)
     }
+
+
 }
